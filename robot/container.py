1fc9df938e741c59855db83904a070a086f064b0
Jon Solera <otherjon@emufarm.org>
2024-01-09 14:39:31 -0800

    working sensor code with no commands2 imports

--- robot/container.py
+++ robot/container.py
@@ -1,13 +1,89 @@
 import wpilib
 import wpilib.deployinfo
-
-import commands2
-import commands2.cmd
-import commands2.button
-
+from wpiutil import SendableBuilder
 from constants import Constants
-from subsystems.digital_sensor_ss import DigitalSensorSS
-from subsystems.analog_sensor_ss import AnalogSensorSS
+
+
+class Sensor:
+    """
+    This class represents a simple digital or analog sensor, plugged into one
+    of the RoboRIO's DIO or AIO ports respectively.
+    """
+    # SENSOR_CLASS must be a Sendable, since we call SmartDashboard.putData()
+    # with it.
+    #
+    SENSOR_CLASS = None
+
+    def __init__(self, io_port, name) -> None:
+        """Create a new Sensor"""
+        super().__init__()
+        self.io_port = io_port
+        self.sensor = self.SENSOR_CLASS(io_port)
+        self.name = name
+        #self.sensor.addChild("P212 Value", self.value())
+        #wpilib.SmartDashboard.putData(f"{name} (#{io_port})", self.sensor)
+
+    def initSendable(self, builder: SendableBuilder) -> None:
+        builder.setSmartDashboardType("Subsystem")
+        builder.addDoubleProperty(".value", lambda: self.value(), lambda: None)
+
+    def value(self):
+        pass
+
+
+class DigitalSensor(Sensor):
+    """
+    This class represents a subsystem consisting of a simple digital sensor,
+    plugged into one of the RoboRIO's Digital I/O (DIO) ports.  The sensor
+    has no external inputs (just what's onboard the sensor), and produces a
+    single binary output.  Examples of these simple digital sensors include:
+      * limit switches
+      * IR proximity sensors
+      * Hall Effect sensors
+      * break-beam sensors
+    """
+    SENSOR_CLASS = wpilib.DigitalInput
+
+    def __init__(self, dio_port, name, invert=False) -> None:
+        """Create a new DigitalSensor"""
+        super().__init__(dio_port, name)
+        self.invert = invert
+
+    def value(self):
+        return not self.sensor.get() if self.invert else self.sensor.get()
+
+
+class AnalogSensor(Sensor):
+    """
+    This class represents a simple analog sensor,
+    plugged into one of the RoboRIO's Analog I/O (AIO) ports.  The sensor
+    has no external inputs (just what's onboard the sensor), and produces a
+    single continuous-voltage output.  Examples of these simple analog sensors
+    include:
+      * ultrasonic sensors
+    """
+    SENSOR_CLASS = wpilib.AnalogInput
+    def __init__(self, aio_port, name, low=0.0, high=1.0, round=None,
+                 scale=True) -> None:
+        """Create a new AnalogSensor"""
+        super().__init__(aio_port, name)
+        self.low = low
+        self.high = high
+        self.round = round
+
+    def raw_unscaled_value(self):
+        return self.sensor.getVoltage()
+
+    def battery_adjusted_value(self):
+        v_batt = wpilib.RobotController.getVoltage5V()
+        return self.raw_unscaled_value() / v_batt
+
+    def value(self):
+        tempval = self.low + (
+            self.battery_adjusted_value() * (self.high - self.low))
+        if self.round:
+            tempval = int(tempval * 10**self.round) / 10**self.round
+        return tempval
 
 
 class RobotContainer:
@@ -28,21 +104,23 @@ class RobotContainer:
         self.deploy_data = wpilib.deployinfo.getDeployData()
 
         # The robot's subsystems
-        self.limit_sw = DigitalSensorSS(Constants.LIMIT_SW_DIO, "Limit Switch")
-        self.ir = DigitalSensorSS(Constants.IR_DIO, "IR Proximity")
-        self.breakbeam = DigitalSensorSS(Constants.BREAK_BEAM_DIO, "Break-Beam")
-        self.hall = DigitalSensorSS(Constants.HALL_EFFECT_DIO, "Hall Effect")
+        self.limit_sw = DigitalSensor(Constants.LIMIT_SW_DIO, "Limit Switch")
+        self.ir = DigitalSensor(Constants.IR_DIO, "IR Proximity", invert=True)
+        self.breakbeam = DigitalSensor(Constants.BREAK_BEAM_DIO, "Break-Beam")
+        self.hall = DigitalSensor(Constants.HALL_EFFECT_DIO, "Hall Effect", invert=True)
         conversion_to_inches = 512.0 / 2.54
-        self.ultrasonic = AnalogSensorSS(
+        self.ultrasonic = AnalogSensor(
             Constants.ULTRASONIC_AIO, "Ultrasonic",
-            max_in=1.0, low=0.0, high=conversion_to_inches)
+            low=0.0, high=conversion_to_inches, round=1)
 
-
-    def getAutonomousCommand(self) -> commands2.Command:
-        """
-        Use this to pass the autonomous command to the main :class:`.Robot`
-        class.
-
-        :returns: the command to run in autonomous mode
-        """
-        return commands2.InstantCommand()
+    def updateSensorData(self):
+        wpilib.SmartDashboard.putBoolean(
+            "ASensorDemo.LimitSwitch", self.limit_sw.value())
+        wpilib.SmartDashboard.putBoolean(
+            "ASensorDemo.IRSensor", self.ir.value())
+        wpilib.SmartDashboard.putBoolean(
+            "ASensorDemo.HallEffectSensor", self.hall.value())
+        wpilib.SmartDashboard.putBoolean(
+            "ASensorDemo.BreakBeamSensor", self.breakbeam.value())
+        wpilib.SmartDashboard.putNumber(
+            "ASensorDemo.UltrasonicRangefinder", self.ultrasonic.value())
--- robot/robot.py
+++ robot/robot.py
@@ -1,14 +1,9 @@
 import wpilib
-import commands2
-import commands2.cmd
-
 import container
 
-class MyRobot(commands2.TimedCommandRobot):
+class MyRobot(wpilib.TimedRobot):
     """
     Our default robot class, pass it to wpilib.run
-    Command v2 robots are encouraged to inherit from TimedCommandRobot, which
-    has an implementation of robotPeriodic which runs the scheduler for you
     """
 
     def robotInit(self) -> None:
@@ -16,7 +11,6 @@ class MyRobot(commands2.TimedCommandRobot):
         This function is run when the robot is first started up and should
         be used for any initialization code.
         """
-        self.autonomousCommand: typing.Optional[commands2.Command] = None
 
         # Instantiate our RobotContainer.  This will perform all our button
         # bindings, and put our autonomous chooser on the dashboard.
@@ -38,32 +32,21 @@ class MyRobot(commands2.TimedCommandRobot):
         This autonomous runs the autonomous command selected by your
         RobotContainer class.
         """
-        self.autonomousCommand = self.container.getAutonomousCommand()
-
-        # schedule the autonomous command (example)
-        if self.autonomousCommand is not None:
-            self.autonomousCommand.schedule()
-        else:
-            wpilib.reportWarning("No autonomous command")
+        wpilib.reportWarning("No autonomous command")
 
     def autonomousPeriodic(self) -> None:
         """This function is called periodically during autonomous"""
+        pass
 
     def teleopInit(self) -> None:
-        # This makes sure that the autonomous stops running when
-        # teleop starts running. If you want the autonomous to
-        # continue until interrupted by another command, remove
-        # this line or comment it out.
-        if self.autonomousCommand is not None:
-            self.autonomousCommand.cancel()
+        pass
 
     def teleopPeriodic(self) -> None:
         """This function is called periodically during operator control"""
+        self.container.updateSensorData()
 
     def testInit(self) -> None:
-        # Cancels all running commands at the start of test mode
-        commands2.CommandScheduler.getInstance().cancelAll()
-
+        pass
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
